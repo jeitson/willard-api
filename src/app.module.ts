61a1f2512a44672f671cf2e993de3cd1df8baa59
isaias-pv <alemais2003312@gmail.com>
2024-07-01 17:35:02 -0500

    Se ubica cual es el usuario super

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import { SystemModule } from './modules/system/system.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 import config from './core/config';
 import { SharedModule } from './core/shared/shared.module';
 import { DatabaseModule } from './core/shared/database/database.module';
 import { SeederService } from './core/common/services/seeder/seeder.service';
 import { RbacGuard } from './modules/auth/guards/rbac.guard';
+import { AllExceptionsFilter } from './core/filters/any-exception.filter';
 
 @Module({
 	imports: [
@@ -28,6 +29,7 @@ import { RbacGuard } from './modules/auth/guards/rbac.guard';
 	],
 	controllers: [],
 	providers: [
+		{ provide: APP_FILTER, useClass: AllExceptionsFilter },
 		{
 			provide: APP_GUARD,
 			useClass: JwtAuthGuard,
