1468e779fb73a51b5fd2264a314774ecd30f822b
Isaias Palacio <iapalacio0312@gmail.com>
2024-07-30 08:03:44 -0500

    Se agrena los campos para descripción

--- src/modules/roles/entities/rol.entity.ts
+++ src/modules/roles/entities/rol.entity.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { CompleteEntity } from "src/core/common/entity/common.entity";
 import { User } from "src/modules/users/entities/user.entity";
 import { Column, Entity, ManyToMany } from "typeorm";
 
 @Entity({ name: 'rol' })
 export class Rol extends CompleteEntity {
+	@ApiProperty({ description: 'Nombre' })
 	@Column({ type: 'varchar', length: 50 })
 	Nombre: string;
 
+	@ApiProperty({ description: 'Descripcion' })
 	@Column({ type: 'varchar', length: 255, default: null, nullable: true })
 	Descripcion: string;
 
