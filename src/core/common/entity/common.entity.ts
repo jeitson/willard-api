df41ad24a48cbe6e9d957cd378d84043cad88130
Isaias Palacio <iapalacio0312@gmail.com>
2024-08-12 14:12:01 -0500

    feat: Habilitar CORS

--- src/core/common/entity/common.entity.ts
+++ src/core/common/entity/common.entity.ts
@@ -10,10 +10,11 @@ import {
 } from 'typeorm';
 
 export abstract class CommonEntity extends BaseEntity {
+	@ApiProperty()
 	@PrimaryGeneratedColumn({ type: 'bigint', name: 'Id' })
 	id: number;
 
-	@ApiHideProperty()
+	@ApiProperty()
 	@CreateDateColumn({
 		nullable: true,
 		type: 'timestamp',
@@ -22,7 +23,7 @@ export abstract class CommonEntity extends BaseEntity {
 	})
 	createdAt: Date;
 
-	@ApiHideProperty()
+	@ApiProperty()
 	@UpdateDateColumn({
 		nullable: true,
 		type: 'timestamp',
@@ -32,19 +33,19 @@ export abstract class CommonEntity extends BaseEntity {
 	})
 	updatedAt: Date;
 
-	@ApiHideProperty()
+	@ApiProperty()
 	@ApiProperty({ description: 'Estado' })
 	@Column({ comment: 'Estado', default: true, name: 'Estado' })
 	status: boolean;
 }
 
 export abstract class CompleteEntity extends CommonEntity {
-	@ApiHideProperty()
+	@ApiProperty()
 	@Exclude()
 	@Column({ update: false, comment: 'Creador', type: 'bigint', nullable: true, name: 'CreadoPor' })
 	createdBy: string;
 
-	@ApiHideProperty()
+	@ApiProperty()
 	@Exclude()
 	@Column({ comment: 'Actualizador', type: 'bigint', nullable: true, name: 'ModificadoPor' })
 	@IsOptional()
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const configService = app.get(ConfigService);
 
+	app.enableCors();
 
 	app.useGlobalPipes(
 		new ValidationPipe({
