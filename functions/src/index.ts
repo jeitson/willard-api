ef6262b80b63b3c80c40f2edad55fc574cd8b73d
palacioia0312 <iapalacio0312@gmail.com>
2024-08-18 21:46:30 -0500

    ...

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,4 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-import * as functions from 'firebase-functions';
+import { onRequest } from 'firebase-functions/v2/https';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './../../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
@@ -26,7 +6,7 @@ import * as express from 'express';
 
 const server = express();
 
-export const api = functions.https.onRequest(async (req, res) => {
+export const api = onRequest(async (req, res) => {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.init();
   app.getHttpAdapter().getInstance().use(req, res);
