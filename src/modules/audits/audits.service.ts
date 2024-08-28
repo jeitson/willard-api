937a0bdd4606d0100c92a78f544dc9431ea6c513
Isaias Palacio <iapalacio0312@gmail.com>
2024-08-28 16:57:10 -0500

    feat: Se integra prueba de otra forma

--- src/modules/audits/audits.service.ts
+++ src/modules/audits/audits.service.ts
@@ -3,10 +3,11 @@ import { AuditDto, AuditQueryDto } from './dto/audit.dto';
 import { Pagination } from 'src/core/helper/paginate/pagination';
 import { Audit } from './entities/audit.entity';
 import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
-import { EntityManager, Like, Repository } from 'typeorm';
+import { DataSource, EntityManager, Like, Repository } from 'typeorm';
 import { paginate } from 'src/core/helper/paginate';
 import { isEmpty } from 'class-validator';
 import { BusinessException } from 'src/core/common/exceptions/biz.exception';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class AuditsService {
@@ -14,7 +15,8 @@ export class AuditsService {
 	constructor(
 		@InjectRepository(Audit)
 		private readonly auditsRepository: Repository<Audit>,
-		@InjectEntityManager() private entityManager: EntityManager
+		@InjectEntityManager() private entityManager: EntityManager,
+		private dataSource: DataSource,
 	) { }
 
 	async findAll({
@@ -22,13 +24,13 @@ export class AuditsService {
 		pageSize,
 		name
 	}: AuditQueryDto): Promise<Pagination<Audit>> {
-		const queryBuilder = this.auditsRepository
-			.createQueryBuilder('auditoria')
-			.leftJoinAndSelect('usuario', 'usuario', 'usuario.id = auditoria.UsuarioId')
-			.addSelect('usuario.Nombre', 'usuarioNombre');
+		const queryBuilder = this.dataSource
+			.getRepository(Audit)
+			.createQueryBuilder('audit')
+			.leftJoinAndSelect(User, 'user', 'user.id = audit.userId');
 
 		if (name) {
-			queryBuilder.andWhere('auditoria.name LIKE :name', { name: `%${name}%` });
+			queryBuilder.andWhere('audit.name LIKE :name', { name: `%${name}%` });
 		}
 
 		return paginate<Audit>(queryBuilder, {
@@ -36,8 +38,6 @@ export class AuditsService {
 			pageSize,
 		});
 	}
-
-
 
 
 	async findOneById(id: number): Promise<Audit | undefined> {
