1e8a597285a047378ed8b09a34891e8da93db4e7
Isaias Palacio <iapalacio0312@gmail.com>
2024-08-29 08:50:05 -0500

    feat: Se parsea los datos antes de devolverlos

--- src/modules/audits/audits.service.ts
+++ src/modules/audits/audits.service.ts
@@ -3,18 +3,15 @@ import { AuditDto, AuditQueryDto } from './dto/audit.dto';
 import { Pagination } from 'src/core/helper/paginate/pagination';
 import { Audit } from './entities/audit.entity';
 import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
-import { DataSource, EntityManager, Like, Repository } from 'typeorm';
+import { DataSource, EntityManager } from 'typeorm';
 import { paginate } from 'src/core/helper/paginate';
-import { BusinessException } from 'src/core/common/exceptions/biz.exception';
 import { User } from '../users/entities/user.entity';
-import { Role } from '../roles/entities/rol.entity';
 
 @Injectable()
 export class AuditsService {
 
 	constructor(
 		@InjectRepository(Audit)
-		private readonly auditsRepository: Repository<Audit>,
 		@InjectEntityManager() private entityManager: EntityManager,
 		private dataSource: DataSource,
 	) { }
@@ -39,17 +36,40 @@ export class AuditsService {
 			queryBuilder.andWhere('audit.name LIKE :name', { name: `%${name}%` });
 		}
 
-		return paginate<Audit>(queryBuilder, {
+		const result = await paginate<Audit>(queryBuilder, {
 			page,
 			pageSize,
 		});
+
+		return {
+			...result,
+			items: result.items.map(audit => {
+				if (audit.response) {
+					audit.response = JSON.parse(audit.response);
+				}
+				if (audit.payload) {
+					audit.payload = JSON.parse(audit.payload);
+				}
+				return audit;
+			})
+		};
 	}
 
 	async findOneById(id: number): Promise<Audit | undefined> {
-		const queryBuilder = this.createBaseQueryBuilder()
-			.where('audit.id = :id', { id });
+		const audit = await this.createBaseQueryBuilder()
+			.where('audit.id = :id', { id })
+			.getOne();
 
-		return queryBuilder.getOne();
+		if (audit) {
+			if (audit.response) {
+				audit.response = JSON.parse(audit.response);
+			}
+			if (audit.payload) {
+				audit.payload = JSON.parse(audit.payload);
+			}
+		}
+
+		return audit;
 	}
 
 	async create(content: AuditDto): Promise<void> {
