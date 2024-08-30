6a44c972e799b1f2017c1430700298a1928dec4e
Isaias Palacio <iapalacio0312@gmail.com>
2024-08-30 15:59:37 -0500

    ...

--- src/core/common/interceptors/transform.interceptor.ts
+++ src/core/common/interceptors/transform.interceptor.ts
@@ -10,12 +10,8 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 
-import { BYPASS_KEY } from '../decorators/bypass.decorator';
 import { ResOp } from '../model/response.model';
 
-/**
- * :: Ajustar el manejo de los resultados de la interfaz de retorno, añadir @Bypass decorador si no es necesario
- */
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
 	constructor(private readonly reflector: Reflector) {}
@@ -24,12 +20,6 @@ export class TransformInterceptor implements NestInterceptor {
 		context: ExecutionContext,
 		next: CallHandler<any>,
 	): Observable<any> {
-		const bypass = this.reflector.get<boolean>(
-			BYPASS_KEY,
-			context.getHandler(),
-		);
-
-		if (bypass) return next.handle();
 
 		return next.handle().pipe(
 			map((data) => {
