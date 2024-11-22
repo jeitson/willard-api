50de18cd210ccafc5321176f3a52a25fbe045ef7
Isaias Palacio <iapalacio0312@gmail.com>
2024-11-22 14:48:04 -0500

    ...

--- src/modules/audit_guia/audit_guia.module.ts
+++ src/modules/audit_guia/audit_guia.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuditGuiaRoute } from './entities/audit_guia-ruta.entity';
 import { AuditGuiaDetail } from './entities/audit_guia_detail.entity';
 import { AuditGuia } from './entities/audit_guia.entity';
+import { ProductsModule } from '../products/products.module';
+import { UsersModule } from '../users/users.module';
 
 const providers = [AuditGuiaService]
 
 @Module({
-	imports: [TypeOrmModule.forFeature([AuditGuia, AuditGuiaDetail, AuditGuiaRoute])],
+	imports: [TypeOrmModule.forFeature([AuditGuia, AuditGuiaDetail, AuditGuiaRoute]), ProductsModule, UsersModule],
 	controllers: [AuditGuiaController],
 	providers,
 	exports: [TypeOrmModule, ...providers],
