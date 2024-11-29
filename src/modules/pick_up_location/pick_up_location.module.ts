f64213df0ec26a0698885b5c5500066e1a92e7c3
palacioia0312 <106626768+palacioia0312@users.noreply.github.com>
2024-11-28 22:52:11 -0500

    Se corrige

--- src/modules/pick_up_location/pick_up_location.module.ts
+++ src/modules/pick_up_location/pick_up_location.module.ts
@@ -5,11 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PickUpLocation } from './entities/pick_up_location.entity';
 import { UsersModule } from '../users/users.module';
 import { ClientsModule } from '../clients/clients.module';
+import { CollectionSitesModule } from '../collection_sites/collection_sites.module';
 
 const providers = [PickUpLocationsService]
 
 @Module({
-	imports: [TypeOrmModule.forFeature([PickUpLocation]), UsersModule, ClientsModule, PickUpLocationModule],
+	imports: [TypeOrmModule.forFeature([PickUpLocation]), UsersModule, ClientsModule, CollectionSitesModule],
 	controllers: [PickUpLocationsController],
 	providers,
 	exports: [TypeOrmModule, ...providers],
