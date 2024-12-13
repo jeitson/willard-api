8279dbed50bfb95e81b17c0ece27cae5c976ee4b
Isaias Palacio <iapalacio0312@gmail.com>
2024-12-13 13:44:09 -0500

    Se exponen los servicios de reporte

--- src/modules/reports_ph/reports_ph.controller.ts
+++ src/modules/reports_ph/reports_ph.controller.ts
@@ -5,13 +5,15 @@ import { ReportsPh } from './entities/reports_ph.entity';
 import { ApiResult } from 'src/core/common/decorators/api-result.decorator';
 import { IdParam } from 'src/core/common/decorators/id-param.decorator';
 import { ReportsPhService } from './reports_ph.service';
+import { Public } from 'src/core/common/decorators/public.decorator';
 
 @ApiTags('Negocio - Reportes')
-@Controller('reports')
+@Controller('reporte_ph')
 export class ReportsPhController {
 	constructor(private readonly reportsService: ReportsPhService) { }
 
 	@Get()
+	@Public()
 	@ApiOperation({ summary: 'Obtener todos los reportes con paginación' })
 	@ApiResult({ type: [ReportsPh], isPage: true })
 	findAll(@Query() query: ReportQueryDto) {
@@ -19,9 +21,9 @@ export class ReportsPhController {
 	}
 
 	@Get(':id')
+	@Public()
 	@ApiOperation({ summary: 'Obtener un reporte por su ID' })
 	findOne(@IdParam('id') id: string): Promise<ReportsPh> {
 		return this.reportsService.findOne(+id);
 	}
-
 }
