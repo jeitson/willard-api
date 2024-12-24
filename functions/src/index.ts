67749133e52211401f9504f2d571dc10dee0a1e5
Isaias Palacio <iapalacio0312@gmail.com>
2024-12-24 08:35:58 -0500

    Se implementa el firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,25 @@
-import { onRequest } from 'firebase-functions/v2/https';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './../../src/app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+/**
+ * Archivo de configuración para redirigir solicitudes HTTP
+ * a tu aplicación NestJS usando Firebase Functions.
+ */
+
+import * as functions from "firebase-functions";
+import express from "express";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./../../dist/app.module";
 
 const server = express();
 
-export const api = onRequest(async (req, res) => {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
-  app.getHttpAdapter().getInstance().use(req, res);
+async function bootstrap() {
+	const adapter = new ExpressAdapter(server);
+	const app = await NestFactory.create(AppModule, adapter);
+
+	await app.init();
+}
+
+bootstrap().catch((error) => {
+	console.error("Error al inicializar NestJS", error);
 });
+
+export const api = functions.https.onRequest(server);
