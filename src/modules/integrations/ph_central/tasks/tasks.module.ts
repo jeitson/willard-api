3aabbe446dce1cd22a782d656f0a2a86e47d5479
Isaias Palacio <iapalacio0312@gmail.com>
2025-01-30 16:45:27 -0500

    Se cambia el task module y ya no se inicializa el module de tareas

--- src/modules/integrations/ph_central/tasks/tasks.module.ts
+++ src/modules/integrations/ph_central/tasks/tasks.module.ts
@@ -2,17 +2,15 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { ApiService } from '../api.service';
 import { ClientsModule } from 'src/modules/clients/clients.module';
-import { Shipment } from 'src/modules/shipments/entities/shipment.entity';
-import { Reception } from 'src/modules/receptions/entities/reception.entity';
 import { HistoryJobsModule } from 'src/modules/history_jobs/history_jobs.module';
-import { ScheduleModule } from '@nestjs/schedule';
+import { ShipmentsModule } from 'src/modules/shipments/shipments.module';
+import { ReceptionsModule } from 'src/modules/receptions/receptions.module';
 
 const providers = [TasksService, ApiService];
 
 @Module({
 	imports: [
-		ScheduleModule.forRoot(),
-		ClientsModule, Shipment, Reception, HistoryJobsModule],
+		ClientsModule, ShipmentsModule, ReceptionsModule, HistoryJobsModule],
 	providers,
 	exports: providers,
 })
