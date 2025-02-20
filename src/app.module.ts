17f29f44051965e5cf3456ede12eddb22687d30f
Isaias Palacio <iapalacio0312@gmail.com>
2025-02-20 09:28:25 -0500

    Se elimina el modulo semilla

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,6 @@ import { ReceptionsModule } from './modules/receptions/receptions.module';
 import { TransporterTravelModule } from './modules/transporter_travel/transporter_travel.module';
 import { FilesModule } from './modules/files/files.module';
 import { ShipmentsModule } from './modules/shipments/shipments.module';
-import { SeedModule } from './modules/seed/seed.module';
 import { ReportsPhModule } from './modules/reports_ph/reports_ph.module';
 import { ApiModule } from './modules/integrations/ph_central/api.module';
 import { IntegrationsModule } from './modules/integrations/integrations.module';
@@ -70,7 +69,6 @@ import { ConciliationModule } from './modules/conciliation/conciliation.module';
 		IntegrationsModule,
 		HistoryJobsModule,
 		ConciliationModule,
-		// SeedModule
 	],
 	controllers: [],
 	providers: [
